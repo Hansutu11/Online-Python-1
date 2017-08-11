c27daa8698c1f3402941db8b680534c119841347
Hans Utu <utuhans@gmail.com>
2017-08-10 19:07:00 -0700

    added Names.py

---
+++ basic.py
@@ -0,0 +1,15 @@
+'''
+Create a dictionary containing some information about yourself. The keys should include name, age, country of birth, favorite language.
+'''
+
+student = {
+    "first_name": "Hans",
+    "age": 32,
+    "birth": "USA",
+    "lang": "Python",
+}
+
+print "My name is",student['first_name']
+print "I am ", student['age']
+print "I was born in", student['birth']
+print "My favorite language is", student['lang']
